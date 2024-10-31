ae52b6940febf1365a969d2f20b04a8058839763
somnath-more <somnath.more19@vit.edu>
2024-10-31 09:28:55 +0530

    removed authenticaion page

--- frontend/src/pages/AuthenticationPage/index.tsx
+++ frontend/src/pages/AuthenticationPage/index.tsx
@@ -1,4 +1,4 @@
-import { useAuth0 } from '@auth0/auth0-react';
+// import { useAuth0 } from '@auth0/auth0-react';
 import { useUserContext } from '../../context';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -11,61 +11,61 @@ import {
 } from 'services/index';
 
 const AuthenticationPage = () => {
-  const { isAuthenticated, user } = useAuth0();
-  const { setUserDetails } = useUserContext();
-  const navigate = useNavigate();
-  useEffect(() => {
-    if (isAuthenticated) {
-      setUserDetails((prev) => ({
-        ...prev,
-        id: 0,
-      }));
-      const { email, name: fullName } = user;
-      const authEmail = `auth0${email}`;
-      const password = 'bagfhgrekbeerehje';
-      try {
-        loginUser({
-          email: authEmail,
-          password,
-        })
-          .then(async (res) => {
-            const userResponse = res;
-            if (userResponse.status !== 404) {
-              localStorage.setItem('token', userResponse.data.token);
-            }
-            const walletResponse = await getWalletDetails(userResponse.data.id);
-            setUserDetails({
-              email: userResponse.data.email,
-              id: userResponse.data.id,
-              walletId: walletResponse.data.id,
-            });
-            navigate('/');
-          })
-          .catch((error) => {
-            if (
-              error.response.data.message ===
-              `User with email:${authEmail}is not present`
-            ) {
-              saveNewUser({
-                firstName: fullName,
-                email: authEmail,
-                password,
-              }).then((res) => {
-                postWalletDetails({
-                  userId: res.data.id,
-                  amount: 34000,
-                }).then((res) => {
-                  console.log(res.data, 'wallet');
-                  navigate('/login');
-                });
-              });
-            }
-          });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  }, [isAuthenticated, user]);
+  // const { isAuthenticated, user } = useAuth0();
+  // const { setUserDetails } = useUserContext();
+  // const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (isAuthenticated) {
+  //     setUserDetails((prev) => ({
+  //       ...prev,
+  //       id: 0,
+  //     }));
+  //     const { email, name: fullName } = user;
+  //     const authEmail = `auth0${email}`;
+  //     const password = 'bagfhgrekbeerehje';
+  //     try {
+  //       loginUser({
+  //         email: authEmail,
+  //         password,
+  //       })
+  //         .then(async (res) => {
+  //           const userResponse = res;
+  //           if (userResponse.status !== 404) {
+  //             localStorage.setItem('token', userResponse.data.token);
+  //           }
+  //           const walletResponse = await getWalletDetails(userResponse.data.id);
+  //           setUserDetails({
+  //             email: userResponse.data.email,
+  //             id: userResponse.data.id,
+  //             walletId: walletResponse.data.id,
+  //           });
+  //           navigate('/');
+  //         })
+  //         .catch((error) => {
+  //           if (
+  //             error.response.data.message ===
+  //             `User with email:${authEmail}is not present`
+  //           ) {
+  //             saveNewUser({
+  //               firstName: fullName,
+  //               email: authEmail,
+  //               password,
+  //             }).then((res) => {
+  //               postWalletDetails({
+  //                 userId: res.data.id,
+  //                 amount: 34000,
+  //               }).then((res) => {
+  //                 console.log(res.data, 'wallet');
+  //                 navigate('/login');
+  //               });
+  //             });
+  //           }
+  //         });
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   }
+  // }, [isAuthenticated, user]);
   return (
     <Stack
       data-testid="loading"
